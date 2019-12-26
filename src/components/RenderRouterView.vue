b86b22dcba80f7446d231a3eb9b81e3d0f945694
kungua <maguahongkun@outlook.com>
2019-12-26 11:10:44 +0800

    update

---
+++ src/components/RenderRouterView.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  name: "RenderRouterView"
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/User/Register.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>gua</div>
+</template>
+
+<script>
+export default {
+  name: "Register"
+};
+</script>
+
+<style scoped>
+
+</style>

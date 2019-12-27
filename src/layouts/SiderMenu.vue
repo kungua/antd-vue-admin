f5bf2503dcfa75af2ac0bacb21ac9d323c46c11b
kungua <maguahongkun@outlook.com>
2019-12-27 11:03:50 +0800

    reset to function submenu

--- src/layouts/SiderMenu.vue
+++ src/layouts/SiderMenu.vue
@@ -36,64 +36,13 @@ export default {
     }
   },
   data() {
-    this.selectedKeysMap = {};
-    this.openKeysMap = {};
-    const menuData = this.getMenuData(this.$router.options.routes);
     return {
       collapsed: false,
-      list: [],
-      menuData,
-      selectedKeys: this.selectedKeysMap[this.$route.path],
-      openKeys: this.collapsed ? [] : this.openKeysMap[this.$route.path]
+      list: []
     };
   },
-  watch: {
-    "$route.path": path => {
-      this.selectedKeys = this.selectedKeysMap[path];
-      this.openKeys = this.collapsed ? [] : this.openKeysMap[path];
-    }
-  },
   methods: {
-    getMenuData(routes = [], parentKeys = [], selectedKeys) {
-      const menuData = [];
-      routes.forEach(route => {
-        if (route.name && !route.hideInMenu) {
-          // 1
-          this.openKeysMap[route.path] = parentKeys;
-          this.selectedKeysMap[route.path] = [route.path || selectedKeys];
-          // 2
-          const newItem = { ...route };
-          delete newItem.children;
-          if (route.children && !route.hideChildrenMenu) {
-            // 3
-            newItem.children = this.getMenuData(route.children, [
-              ...parentKeys,
-              route.path
-            ]);
-          } else {
-            // 4
-            newItem.children = this.getMenuData(
-              route.children,
-              selectedKeys ? parentKeys : [...parentKeys, route.path],
-              selectedKeys || route.path
-            );
-          }
-          menuData.push(newItem);
-        } else if (
-          !route.hideInMenu &&
-          !route.hideChildrenMenu &&
-          route.children
-        ) {
-          // 1
-          menuData.push(
-            ...this.getMenuData(route.children, [...parentKeys, route.path])
-          );
-        }
-      });
-      console.log("selectedKeysMap");
-      console.log(this.selectedKeysMap);
-      return menuData;
-    }
+    getMenuData() {}
   }
 };
 </script>
--- src/layouts/SubMenu.vue
+++ src/layouts/SubMenu.vue
@@ -1,34 +1,19 @@
-<template>
-  <a-sub-menu :key="menuInfo.path" v-bind="$props" v-on="$listeners">
+<template functional>
+  <a-sub-menu :key="props.menuInfo.key">
     <span slot="title">
-      <a-icon v-if="menuInfo.meta.icon" :type="menuInfo.meta.icon" />
-      <span>
-        {{ menuInfo.meta.title }}
-      </span>
+      <a-icon type="mail" /><span>{{ props.menuInfo.title }}</span>
     </span>
-    <template v-for="item in menuInfo.children">
+    <template v-for="item in props.menuInfo.children">
       <a-menu-item v-if="!item.children" :key="item.key">
-        <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
-        <span>{{ item.meta.title }}</span>
+        <a-icon type="pie-chart" />
+        <span>{{ item.title }}</span>
       </a-menu-item>
-      <sub-menu v-else :key="item.path" :menu-info="item" />
+      <sub-menu v-else :key="item.key" :menu-info="item" />
     </template>
   </a-sub-menu>
 </template>
 <script>
-import { Menu } from "ant-design-vue";
-
 export default {
-  name: "SubMenu",
-  // must add isSubMenu: true
-  isSubMenu: true,
-  props: {
-    ...Menu.SubMenu.props,
-    // Cannot overlap with properties within Menu.SubMenu.props
-    menuInfo: {
-      type: Object,
-      default: () => ({})
-    }
-  }
+  props: ["menuInfo"]
 };
 </script>
